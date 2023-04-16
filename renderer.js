c2b9158aae680f225bacb319b7fa1bd8fae0664b
Mudassir Malik <Malikjrw147@gmail.com>
2023-04-16 12:04:22 +0500

    First commit v1

--- renderer.js
+++ renderer.js
@@ -1,165 +1,123 @@
 let { remote } = require("electron");
 // console.log(process.versions.electron);
 
-const { PosPrinter } = remote.require("electron-pos-printer");
+const { PosPrinter, } = remote.require("electron-pos-printer");
 // const {PosPrinter} = require("electron-pos-printer"); //dont work in production (??)
 
 const path = require("path");
 
 let webContents = remote.getCurrentWebContents();
 let printers = webContents.getPrinters(); //list the printers
-console.log(printers);
 
-printers.map((item, index) => {
+printers.filter(p => p.name.toLowerCase().includes("rollo")).map((item, index) => {
   //write in the screen the printers for choose
-  document.getElementById("list_printers").innerHTML +=
-    ' <input type="radio" id="printer_' +
-    index +
-    '" name="printer" value="' +
-    item.name +
-    '"><label for="printer_' +
-    index +
-    '">' +
-    item.name +
-    "</label><br>";
+  document.getElementById("list_printers").innerHTML += `
+  <div style="display: flex;align-items: center;">
+  <p id=printer_${index}>${item.name}</p> 
+  <input onclick="print()" type="button" value="print" style="margin-left: 10px" />
+  </div>`;
 });
 
-
-const data = [
-  {
-    type: "text", // 'text' | 'barCode' | 'qrCode' | 'image' | 'table
-    value: "||---",
-    // style: `text-align:left;`,
-    style: { "font-size": "12px" },
-  },
-  {
-    type: "text", // 'text' | 'barCode' | 'qrCode' | 'image' | 'table
-    value: "HEADER",
-    // style: `text-align:center;`,
-    style: { "font-weight": "700", "font-size": "18px" },
-  },
-  {
-    type: "image",
-    path: path.join(__dirname, "assets/img_test.png"), // file path
-    position: "center", // position of image: 'left' | 'center' | 'right'
-    width: "auto", // width of image in px; default: auto
-    height: "60px", // width of image in px; default: 50 or '50px'
-  },
-  {
-    type: "text", // 'text' | 'barCode' | 'qrCode' | 'image' | 'table'
-    value:
-      "Lorem ipsum<br><br> . , ; : ( ) - + = ! # % \" ' <br><br> ã Ã ç Ç $ & @ ê Ê í Í<br><br> 0 1 2 3 4 5 6 7 8 9 <br>a b c d e f g h i j k l m n o p q r s t u v w x y z<br>A B C D E F G H I J K L M N O P Q R S T U V W X Y Z<br><br><hr><br>elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation \n ullamco \n laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum<br>",
-
-    style: {
-      "font-size": "12px",
-      "font-family": "sans-serif",
-      "text-align": "center",
-    },
-  },
-  {
-    type: "barCode", // Do you think the result is ugly? Me too. Try use an image instead...
-    value: "HB4587896",
-    height: 12,
-    width: 1,
-    displayValue: true, // Display value below barcode
-    fontsize: 8,
-  },
-  {
-    type: "qrCode",
-    value: "https://github.com/fssonca",
-    height: 80,
-    width: 80,
-    // style: "margin-left:50px",
-  },
-  {
-    type: "text", // 'text' | 'barCode' | 'qrCode' | 'image' | 'table
-    value: "---||",
-    // style: `text-align:right;`,
-    style: { "font-size": "12px" },
-  },
-];
-
-function date() {
-  const x = new Date();
-
-  const y = "0" + x.getHours();
-  const z = "0" + x.getMinutes();
-  const s = "0" + x.getSeconds();
-  const h = "0" + x.getDate();
-  const ano = x.getFullYear().toString().substr(-2);
-  const ms = x.getMonth();
-  const meses = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "June",
-    "July",
-    "Aug",
-    "Sept",
-    "Oct",
-    "Nov",
-    "Dec",
-  ];
-
-  return (
-    y.substr(-2) +
-    ":" +
-    z.substr(-2) +
-    ":" +
-    s.substr(-2) +
-    " -  " +
-    h.substr(-2) +
-    "/" +
-    meses[ms]
-  );
-}
-
 function print() {
-  let printerName;
-  let widthPage;
+  let printerName = 'Rollo X1060';
 
   var p = document.getElementsByName("printer");
-  var w = document.getElementsByName("width");
 
-  for (var i = 0, length = p.length; i < length; i++) {
-    if (p[i].checked) {
-      printerName = p[i].value;
-
-      break;
-    }
-  }
-
-  for (var i = 0, length = w.length; i < length; i++) {
-    if (w[i].checked) {
-      widthPage = w[i].value;
-
-      break;
-    }
-  }
-
-  console.log(printerName, widthPage);
 
   const options = {
-    preview: false, // Preview in window or print
-    width: widthPage, //  width of content body
+    preview: true, // Preview in window or print
+    width: '200px', //  width of content body
     margin: "0 0 0 0", // margin of content body
     copies: 1, // Number of copies to print
     printerName: printerName, // printerName: string, check it at webContent.getPrinters()
     timeOutPerLine: 400,
     silent: true,
+    pageSize: { height: window.screen.height * .5, width: window.screen.width * .5 }  // page size
+
   };
 
   const now = {
     type: "text",
-    value: "" + date(),
+    value: "" + Date.now(),
     style: { "font-size": "12px", "text-align": "center", "font-family": "sans-serif" },
   };
+  const data = [
+    {
+      type: 'image',
+      url: 'https://randomuser.me/api/portraits/men/43.jpg',     // file path
+      position: 'center',                                  // position of image: 'left' | 'center' | 'right'
+      // width: '160px',                                           // width of image in px; default: auto
+      // height: '60px',                                          // width of image in px; default: 50 or '50px'
+    },
+    // {
+    //   type: 'text',                                       // 'text' | 'barCode' | 'qrCode' | 'image' | 'table
+    //   value: 'SAMPLE HEADING',
+    //   style: { fontWeight: "700", textAlign: 'center', fontSize: "24px" }
+    // }, 
+    // {
+    //   type: 'text',                       // 'text' | 'barCode' | 'qrCode' | 'image' | 'table'
+    //   value: 'Secondary text',
+    //   style: { textDecoration: "underline", fontSize: "10px", textAlign: "center", color: "red" }
+    // }, {
+    //   type: 'barCode',
+    //   value: '023456789010',
+    //   height: 40,                     // height of barcode, applicable only to bar and QR codes
+    //   width: 2,                       // width of barcode, applicable only to bar and QR codes
+    //   displayValue: true,             // Display value below barcode
+    //   fontsize: 12,
+    // }, {
+    //   type: 'qrCode',
+    //   value: 'https://github.com/Hubertformin/electron-pos-printer',
+    //   height: 55,
+    //   width: 55,
+    //   style: { margin: '10 20px 20 20px' }
+    // }, {
+    //   type: 'table',
+    //   // style the table
+    //   style: { border: '1px solid #ddd' },
+    //   // list of the columns to be rendered in the table header
+    //   tableHeader: ['Animal', 'Age'],
+    //   // multi dimensional array depicting the rows and columns of the table body
+    //   tableBody: [
+    //     ['Cat', 2],
+    //     ['Dog', 4],
+    //     ['Horse', 12],
+    //     ['Pig', 4],
+    //   ],
+    //   // list of columns to be rendered in the table footer
+    //   tableFooter: ['Animal', 'Age'],
+    //   // custom style for the table header
+    //   tableHeaderStyle: { backgroundColor: '#000', color: 'white' },
+    //   // custom style for the table body
+    //   tableBodyStyle: { 'border': '0.5px solid #ddd' },
+    //   // custom style for the table footer
+    //   tableFooterStyle: { backgroundColor: '#000', color: 'white' },
+    // }, {
+    //   type: 'table',
+    //   style: { border: '1px solid #ddd' },             // style the table
+    //   // list of the columns to be rendered in the table header
+    //   tableHeader: [{ type: 'text', value: 'People' }, { type: 'image', path: path.join(__dirname, 'icons/animal.png') }],
+    //   // multi-dimensional array depicting the rows and columns of the table body
+    //   tableBody: [
+    //     [{ type: 'text', value: 'Marcus' }, { type: 'image', url: 'https://randomuser.me/api/portraits/men/43.jpg' }],
+    //     [{ type: 'text', value: 'Boris' }, { type: 'image', url: 'https://randomuser.me/api/portraits/men/41.jpg' }],
+    //     [{ type: 'text', value: 'Andrew' }, { type: 'image', url: 'https://randomuser.me/api/portraits/men/23.jpg' }],
+    //     [{ type: 'text', value: 'Tyresse' }, { type: 'image', url: 'https://randomuser.me/api/portraits/men/53.jpg' }],
+    //   ],
+    //   // list of columns to be rendered in the table footer
+    //   tableFooter: [{ type: 'text', value: 'People' }, 'Image'],
+    //   // custom style for the table header
+    //   tableHeaderStyle: { backgroundColor: 'red', color: 'white' },
+    //   // custom style for the table body
+    //   tableBodyStyle: { 'border': '0.5px solid #ddd' },
+    //   // custom style for the table footer
+    //   tableFooterStyle: { backgroundColor: '#000', color: 'white' },
+    // },
+  ]
 
   const d = [...data, now];
 
-  if (printerName && widthPage) {
+  if (printerName) {
     PosPrinter.print(d, options)
       .then(() => { })
       .catch((error) => {
