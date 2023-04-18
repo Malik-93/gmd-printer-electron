1b1a08a6cbc4f9aeebbfbdcc1d6050ec87fef9da
Mudassir Malik <Malikjrw147@gmail.com>
2023-04-18 18:05:22 +0500

    some updates

--- forge.config.js
+++ forge.config.js
@@ -1,23 +1,22 @@
 module.exports = {
-    packagerConfig: {},
-    rebuildConfig: {},
-    makers: [
-      {
-        name: '@electron-forge/maker-squirrel',
-        config: {},
-      },
-      {
-        name: '@electron-forge/maker-zip',
-        platforms: ['darwin', 'win32', 'linux'],
-      },
-      {
-        name: '@electron-forge/maker-deb',
-        config: {},
-      },
-      {
-        name: '@electron-forge/maker-rpm',
-        config: {},
-      },
-    ],
-  };
-  
+  packagerConfig: {},
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      config: {}
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
+  ],
+};
